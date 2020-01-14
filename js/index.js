5c7ea0bc46f650b8ed58de0b6c4371555a38301d
DariaHuesch <59878338+DariaHuesch@users.noreply.github.com>
2020-01-14 16:05:25 +0100

    Erste Version

---
+++ js/index.js
@@ -0,0 +1,42 @@
+function locationupdaten() {
+    navigator.geolocation.getCurrentPosition(
+        function(pos) {
+            let card = document.getElementById("locationcard");
+            card.innerText = "lat = " + pos.coords.latitude + 
+                         "long = " + pos.coords.longitude;
+        }
+        , 
+        function(err) {
+            alert("Unable to retrieve position: " + err)
+        }
+    );
+}
+
+
+function bodyGeladen(){ //die funktion wird aufgerufen, sobald alles einmal geladen hat, d.h. bis unten in der Funktion und es wird abgefragt, ob wir uns gerade in der Nativen App befinden oder im Browser
+    if(!window.usingCordova){ 
+        initApp();
+    }
+}
+ 
+function initApp(){ 
+    console.log("Starting framework7 app...")
+    var app = new Framework7({
+        root: '#app',
+        name: 'My App',
+        id: 'com.myapp.test',
+        panel: {
+        swipe: 'left',
+        }
+     });
+
+     if(window.usingCordova){ //schaut, ob es ein Broswer ist oder ob es Cordova ist
+        console.log("In der nativen Anwendung")
+    }else { //Browser
+        console.log("Im Browser")
+    }
+
+
+}
+
+document.addEventListener('deviceready',initApp,false);
