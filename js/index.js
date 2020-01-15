6c4c1b0eb1b1387bb5c3bc839b6ec5c58a87acbf
DariaHuesch <59878338+DariaHuesch@users.noreply.github.com>
2020-01-15 15:14:20 +0100

    ...

--- js/index.js
+++ js/index.js
@@ -1,42 +1,55 @@
 function locationupdaten() {
     navigator.geolocation.getCurrentPosition(
-        function(pos) {
+        function (pos) {
             let card = document.getElementById("locationcard");
-            card.innerText = "lat = " + pos.coords.latitude + 
-                         "long = " + pos.coords.longitude;
+            card.innerText = "lat = " + pos.coords.latitude +
+                "long = " + pos.coords.longitude;
         }
-        , 
-        function(err) {
+        ,
+        function (err) {
             alert("Unable to retrieve position: " + err)
         }
     );
 }
 
+function callNumber(number) {
+    if (!number)
+        number = "+4962141050"
+    if (window.usingCordova && window.plugins.CallNumber) {
+        window.plugins.CallNumber.callNumber((result) => {
+            console.log("Number dialed")
+        }, (result) => {
+            alert("Unable to call number" + result)
+        }, number, true);
+    }
+}
 
-function bodyGeladen(){ //die funktion wird aufgerufen, sobald alles einmal geladen hat, d.h. bis unten in der Funktion und es wird abgefragt, ob wir uns gerade in der Nativen App befinden oder im Browser
-    if(!window.usingCordova){ 
+
+function bodyGeladen() { //die funktion wird aufgerufen, sobald alles einmal geladen hat, d.h. bis unten in der Funktion und es wird abgefragt, ob wir uns gerade in der Nativen App befinden oder im Browser
+    if (!window.usingCordova) {
         initApp();
     }
 }
- 
-function initApp(){ 
+
+function initApp() {
     console.log("Starting framework7 app...")
     var app = new Framework7({
         root: '#app',
         name: 'My App',
         id: 'com.myapp.test',
         panel: {
-        swipe: 'left',
+            swipe: 'left',
         }
-     });
+    });
 
-     if(window.usingCordova){ //schaut, ob es ein Broswer ist oder ob es Cordova ist
+    if (window.usingCordova) { //schaut, ob es ein Broswer ist oder ob es Cordova ist
         console.log("In der nativen Anwendung")
-    }else { //Browser
+    } else { //Browser
         console.log("Im Browser")
     }
 
 
 }
 
-document.addEventListener('deviceready',initApp,false);
+document.addEventListener('deviceready', initApp, false);
+
